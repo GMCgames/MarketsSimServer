2713960ce5c9859b14cb4eb4383b10813a9c1477
carfaper <er.carlos@gmail.com>
2016-02-20 12:41:58 +0100

    MarketsSimService para canalizar las peticiones de obtención de datos bursátiles y todo el modelo necesario para su soporte. Nuevo endpoint "/MarketsSimQuery" para obtener los datos en Json

--- src/main/java/controller/StatusController.java
+++ src/main/java/controller/StatusController.java
@@ -1,22 +1,49 @@
 package controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import bll.StatusService;
+import bll.MarketsSimService;
 import bll.model.Status;
+import bll.model.MarketsSimSymbol;
+
 
 @RestController
 public class StatusController {
 	
 	private StatusService _statusService;
+	private MarketsSimService _marketsSimService;
 	
 	public StatusController() {
 		_statusService = new StatusService();
+		_marketsSimService = new MarketsSimService();
+
 	}
 	
 	@RequestMapping("/status")
 	public Status status(){
 		return _statusService.getCurrentStatus();
 	}
+
+	@RequestMapping("/MarketsSimQuery")
+	public MarketsSimSymbol getMarketsSimSymbol(
+			@RequestParam(value="country", defaultValue="ES") String country,
+			@RequestParam(value="market", defaultValue="IBEX35") String market,
+			@RequestParam(value="ticker", defaultValue="ACCIONA") String ticker,
+			@RequestParam(value="rt", defaultValue="no") String rt)
+	{
+		MarketsSimSymbol mss=null;
+		boolean isRealtime;
+		
+		isRealtime = (rt.compareTo("yes") == 0);
+
+		System.out.println(country + market + ticker + isRealtime);
+		mss = _marketsSimService.getMarketsSimSymbol(country, market, ticker, isRealtime);
+		
+		
+		return mss;
+	}
+			
 }

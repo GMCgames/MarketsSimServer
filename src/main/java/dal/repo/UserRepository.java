d09bc58adbb25a159ace6ff782e6505b01485625
elbecita <elbe.jam@gmail.com>
2016-03-28 22:28:29 +0200

    Set up authentication: basic classes & configuration

--- src/main/java/dal/repo/UserRepository.java
+++ src/main/java/dal/repo/UserRepository.java
@@ -27,7 +27,7 @@ public class UserRepository {
 		// Initialise Users table mock with 2 only valid users
 		_userTableMock = new ArrayList<UserDAO>();
 		_userTableMock.add(new UserDAO("user1@gmail.com", "12345", "user1"));
-		_userTableMock.add(new UserDAO("user2@gmail.com", "67890", "user1"));
+		_userTableMock.add(new UserDAO("user2@gmail.com", "67890", "user2"));
 	}
 }
 
